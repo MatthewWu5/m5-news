5d146c2fb2a35074841ad27b2c7012a233494d75
Matthew Wu <wum@cn.hsntech.int>
2017-06-21 14:33:49 +0800

    url property change

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ let url = require('./url');
 var express = require('express');
 var router = express.Router();
 
-router.post(url.c2, api.getTestData1);
-router.post(url.c2, api.getTestData2);
+router.post(url.getData1, api.getTestData1);
+router.post(url.getData2, api.getTestData2);
 
 module.exports = router;
