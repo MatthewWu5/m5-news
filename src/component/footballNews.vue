9af9ef1009ef4348a0aa991d55edff579f5267fc
Matthew Wu <wum@cn.hsntech.int>
2017-08-15 16:05:11 +0800

    refine code

--- src/component/footballNews.vue
+++ src/component/footballNews.vue
@@ -20,8 +20,8 @@
           <button @click="OnGoPageClick">Go Page</button>
           <input type="checkbox" v-model="original" style="width:30px;height:30px">Original</input>
           <span style="color: #a8c6e2;font-weight: bold;margin-left:10px">
-                                {{'Min Time: '+_currentMinDateString}}
-                              </span>
+                                  {{'Min Time: '+_currentMinDateString}}
+                                </span>
         </div>
       </div>
   
@@ -175,7 +175,7 @@ export default {
     },
 
     moreNewsOnClick: function () {
-      this.getMore2();
+      this.getMore();
     },
 
     formatRequestDate: function (minDate, index) {
@@ -188,7 +188,7 @@ export default {
       return date.getFullYear() + '-' + month + '-' + day;
     },
 
-    getMore2: function () {
+    getMore: function () {
       var self = this;
       self.requestStatus = 'loading...';
       axios.post(url.getMoreData, { currentMinDate: self.currentMinDate, intervalDay: self.intervalDay })
@@ -198,141 +198,14 @@ export default {
           self.$nextTick(function () {
             for (let i = 0; i < objKeys.length; i++) {
               var current = self.footballNews.find(x => x.category == objKeys[i])
-              console.log(current)
               current.news = current.news.concat(source[objKeys[i]].news)
             }
             self.currentMinDate = new Date(resp.data.data.minDate)
+            self.requestStatus = '';
           })
-          // self.$nextTick(function () {
-          //   for (assembleProp in source) {
-          //     console.log('assembleProp', assembleProp)
-          //     self.footballNews[assembleProp].news = self.footballNews[assembleProp].news.concat(source[assembleProp].news)
-          //   }
-          //   self.currentMinDate = new Date(resp.data.data.minDate)
-          // })
         }).catch(err => {
           console.error(err)
         })
-    },
-
-    getMore: function () {
-      var self = this;
-      self.requestStatus = 'loading...';
-      self.$nextTick(function () {
-        if (self.intervalDay > 30) self.intervalDay = 30;
-        var promiseArray = [];
-        for (var i = 1; i < self.intervalDay + 1; i++) {
-          promiseArray.push(self.moreRequest(self.formatRequestDate(self.currentMinDate, i)));
-        }
-        Promise.all(promiseArray).then(function (resps) {
-          // for(let i = 0; i<resps.length;i++){
-          //   if (resps[i]) self.appendNews(self.footballNews, resps[i])
-          // }
-          resps.forEach(x => {
-            if (x) self.appendNews(self.footballNews, x)
-          })
-
-          self.requestStatus = '';
-          //in order to call computed prop
-          var currentDate = self.currentMinDate;
-          currentDate.setDate(currentDate.getDate() - self.intervalDay);
-          self.currentMinDate = new Date(currentDate.getFullYear() + '.' + self.formatTime(currentDate))
-        }).catch(err => {
-          console.error('Promise.all:', err)
-        })
-      })
-    },
-    moreRequest: function (date) {
-      console.log('more request date:', date)
-      var self = this;
-      return new Promise((resolve, reject) => {
-        axios.post(url.getJsonData, { host: 'news.zhibo8.cc', path: '/zuqiu/json/' + date + '.htm' })
-          .then(resp => {
-            var data = resp.data.data;
-            let footballData = data.video_arr.filter(x => x.type == 'zuqiu');
-            var result = self.assembleFootballData(footballData)
-            resolve(self.toArray(result))
-          }).catch(err => {
-            reject(err);
-          })
-      }).catch(err => {
-        console.error(err);
-      })
-    },
-    getFormatNewsData: function (category, rawData, urlType = 'news') {
-      var self = this;
-      var result = rawData.map(function (x) {
-        return {
-          title: x.title,
-          url: (urlType == 'news' ? 'https://news.zhibo8.cc' : 'https://www.zhibo8.cc') + x.url,
-          host: (urlType == 'news' ? 'news.zhibo8.cc' : 'www.zhibo8.cc'),
-          path: x.url,
-          time: self.formatTime(x.updatetime),
-          updatetime: x.updatetime,
-          lable: x.lable,
-          isLeo: x.title.indexOf('梅西') != -1 || x.title.toLowerCase().indexOf('messi') != -1
-        }
-      }).sort((x, y) => { return new Date(x.updatetime) < new Date(y.updatetime) ? 1 : -1; })
-
-      return {
-        category: category,
-        news: result
-      }
-    },
-    isTop5League: function (str) {
-      if (str.indexOf('英超') != -1 || str.indexOf('西甲') != -1 || str.indexOf('德甲') != -1 || str.indexOf('法甲') != -1 || str.indexOf('意甲') != -1) {
-        return true;
-      }
-      return false;
-    },
-    indexOf: function (sourceStr, indexStr) {
-      if (sourceStr.indexOf(indexStr) != -1) {
-        return true;
-      }
-    },
-    filterTabData: (str) => {
-      if (str.indexOf('官方') != -1 || str.indexOf('盘点') != -1 || str.indexOf('趣图') != -1 || str.indexOf('GIF-') != -1) {
-        return false;
-      }
-      return true;
-    },
-    formatTime: function (dateTime) {
-      let date = new Date(dateTime)
-      return date.getMonth() + 1 + '.' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
-    },
-    assembleFootballData: function (footballData) {
-      var self = this;
-      let top5LeagueData = footballData.filter(x => self.isTop5League(x.lable))
-      let internationalData = top5LeagueData.filter(x => self.filterTabData(x.title))
-      let international_official = top5LeagueData.filter(x => self.indexOf(x.title, '官方'))
-      let international_conclusion = top5LeagueData.filter(x => self.indexOf(x.title, '盘点'))
-      let international_funnyTime = footballData.filter(x => self.indexOf(x.title, '趣图'))
-
-      let _international = self.getFormatNewsData(const_news.Category.News, internationalData)
-      let _official = self.getFormatNewsData(const_news.Category.Official, international_official)
-      let _conclusion = self.getFormatNewsData(const_news.Category.Conclusion, international_conclusion)
-      let _funnyTime = self.getFormatNewsData(const_news.Category.FunnyTime, international_funnyTime)
-
-      return {
-        _international: _international,
-        _official: _official,
-        _conclusion: _conclusion,
-        _funnyTime: _funnyTime,
-      }
-    },
-    toArray: (obj) => {
-      var arr = [];
-      for (var item in obj) {
-        arr.push(obj[item]);
-      }
-      return arr;
-    },
-    appendNews: (footballNews, assembleItem) => {
-      for (prop of assembleItem) {
-        footballNews.filter(x => x.category == prop.category).forEach(x => {
-          if (x) x.news = x.news.concat(prop.news)
-        })
-      }
     },
   },
   created: function () {
@@ -348,33 +221,32 @@ export default {
       }).catch(err => {
         console.error(err)
       })
-    return;
 
     //Client
-    var ajaxPromise = new Promise(function (resolve, reject) {
-      $.ajax({
-        url: 'https://m.zhibo8.cc/json/hot/24hours.htm',
-        dataType: "json",
-        success: function (data) {
-          let videoData = data.video.filter(x => x.type == 'zuqiujijin' && self.isTop5League(x.lable));
-          let footballData = data.news.filter(x => x.type == 'zuqiu');
-          var result = self.assembleFootballData(footballData)
-          let _videoData = self.getFormatNewsData(const_news.Category.Video, videoData, 'video')
-          let minDate = new Date()
-          let newsList = result._international.news;
-          if (newsList.length > 0) minDate = new Date(newsList[newsList.length - 1].updatetime)
-          let resultArray = self.toArray(result)
-          resultArray.push(_videoData)
-          resolve({ source: resultArray, minDate: minDate })
-        }
-      })
-    })
+    // var ajaxPromise = new Promise(function (resolve, reject) {
+    //   $.ajax({
+    //     url: 'https://m.zhibo8.cc/json/hot/24hours.htm',
+    //     dataType: "json",
+    //     success: function (data) {
+    //       let videoData = data.video.filter(x => x.type == 'zuqiujijin' && self.isTop5League(x.lable));
+    //       let footballData = data.news.filter(x => x.type == 'zuqiu');
+    //       var result = self.assembleFootballData(footballData)
+    //       let _videoData = self.getFormatNewsData(const_news.Category.Video, videoData, 'video')
+    //       let minDate = new Date()
+    //       let newsList = result._international.news;
+    //       if (newsList.length > 0) minDate = new Date(newsList[newsList.length - 1].updatetime)
+    //       let resultArray = self.toArray(result)
+    //       resultArray.push(_videoData)
+    //       resolve({ source: resultArray, minDate: minDate })
+    //     }
+    //   })
+    // })
 
-    var self = this;
-    ajaxPromise.then(function (res) {
-      self.footballNews = res.source;
-      self.currentMinDate = res.minDate;
-    })
+    // var self = this;
+    // ajaxPromise.then(function (res) {
+    //   self.footballNews = res.source;
+    //   self.currentMinDate = res.minDate;
+    // })
   }
 }
 </script>
