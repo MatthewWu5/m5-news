11710da9b19e3330a1bb36a8fb85764e3242585e
Matthew Wu <wum@cn.hsntech.int>
2017-10-16 13:26:41 +0800

    update dist folder

--- router.js
+++ router.js
@@ -9,10 +9,10 @@ for (prop in url) {
     router.post(url[prop], core[prop])
 }
 router.get('/', function(req, res){
-  res.sendFile(__dirname + '/publish/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 router.get('/app.js', function(req, res){
-  res.sendFile(__dirname + '/publish/app.js');
+  res.sendFile(__dirname + '/dist/app.js');
 });
 
 module.exports = router;
